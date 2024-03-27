513feb6488c8502b2c137088c323976bbc93b032
Xinwei Xiong <3293172751NSS@gmail.com>
2024-03-27 14:26:16 +0800

    Create formitychecker.go
    Signed-off-by: Xinwei Xiong <3293172751NSS@gmail.com>

---
+++ formitychecker.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/openimsdk/open-im-server/tools/formitychecker/checker"
+	"github.com/openimsdk/open-im-server/tools/formitychecker/config"
+)
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "Path to the configuration file")
+	flag.Parse()
+
+	if configPath == "" {
+		configPath = os.Getenv("CONFIG_PATH")
+	}
+
+	if configPath == "" {
+		configPath = "config.yaml"
+		if _, err := os.Stat(".github/formitychecker.yaml"); err == nil {
+			configPath = ".github/formitychecker.yaml"
+		}
+	}
+
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
+	c := &checker.Checker{Config: cfg}
+	err = c.Check()
+	if err != nil {
+		fmt.Println("Error during check:", err)
+		os.Exit(1)
+	}
+
+	// if len(c.Errors) > 0 {
+	// 	fmt.Println("Found errors:")
+	// 	for _, errMsg := range c.Errors {
+	// 		fmt.Println("-", errMsg)
+	// 	}
+	// 	os.Exit(1)
+	// }
+
+	summaryJSON, err := json.MarshalIndent(c.Summary, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling summary:", err)
+		return
+	}
+
+	fmt.Println(string(summaryJSON))
+}
